cb33cb408b2b736393112c511f8b1beec168328b
SiarheiBalt <jh-11@yandex.by>
2021-10-12 17:45:37 +0300

    feat: add button close/open for ReserveForm

--- src/components/commons/reserveItem/ReserveItem.jsx
+++ src/components/commons/reserveItem/ReserveItem.jsx
@@ -1,13 +1,32 @@
+import { useState } from 'react';
+import { Button } from '../button/Button';
 import ReserveForm from '../ReserveForm/ReserveForm';
 import Shedule from '../shedule/Shedule';
 import cl from './ReserveItem.module.css';
 
 const ReserveItem = ({ dates, name, reserveSubmit, type }) => {
+  const [isFormOpen, setIsFormOpen] = useState(false);
+  const changeStatusForm = () => {
+    setIsFormOpen(!isFormOpen);
+  };
+
   return (
     <div className={cl.room}>
       <h2 className={cl.title}>Комната {name}</h2>
+      <div className={cl.button__container}>
+        <Button
+          action={
+            isFormOpen
+              ? 'Закрыть форму для резервирования'
+              : 'Открыть форму для резервирования'
+          }
+          onClick={changeStatusForm}
+        />
+      </div>
       <Shedule timeData={dates} />
-      <ReserveForm reserveSubmit={reserveSubmit} name={name} type={type} />
+      {isFormOpen && (
+        <ReserveForm reserveSubmit={reserveSubmit} name={name} type={type} />
+      )}
     </div>
   );
 };
