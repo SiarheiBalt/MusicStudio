e7d3462cdb102af15ada63135e33bdcf2379a561
SiarheiBalt <jh-11@yandex.by>
2021-10-28 18:08:03 +0300

    feat: component specification

--- src/components/container/instruments/instrument/Instrument.jsx
+++ src/components/container/instruments/instrument/Instrument.jsx
@@ -1,10 +1,14 @@
-import cl from './Instrument.module.css';
 import { Button } from './../../../commons/button/Button';
 import { useState } from 'react';
+
 import Modal from '../modal/Modal';
+import Specifications from './specifications/Specifications';
+
+import cl from './Instrument.module.css';
 
 const Instrument = ({ image, specifications, dates, instrumentId }) => {
   const [isModal, setModal] = useState(false);
+  const [isMouseOver, setIsMouseOver] = useState(false);
 
   const openModal = () => setModal(true);
   const closeModal = () => setModal(false);
@@ -13,12 +17,19 @@ const Instrument = ({ image, specifications, dates, instrumentId }) => {
     <Modal closeModal={closeModal} dates={dates} instrumentId={instrumentId} />
   );
 
+  const showSpecifications = isMouseOver && <Specifications />;
+
   const altText = 'image not find';
 
   return (
     <>
       <div className={`${cl.instrument} `}>
-        <div className={cl.image__container}>
+        <div
+          className={cl.image__container}
+          onMouseEnter={() => setIsMouseOver(true)}
+          onMouseLeave={() => setIsMouseOver(false)}
+        >
+          {showSpecifications}
           <img className={cl.image} src={image} alt={altText} />
         </div>
         <div className={cl.form}>
